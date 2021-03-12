12df6c372d731dd19f1c998a71011fa734b33335
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-03-12 14:02:45 -0800

    fixing

--- app.js
+++ app.js
@@ -1,36 +1,6 @@
 const express = require("express");
 const app = express();
 const { cp } = require('./db/connection.js');
-const promise_mysql = require('promise-mysql');
-
-
-
-const farms = [
-    {
-        farmId: "1",
-        farmName: "farm num 1",
-        farmOwner: "Mr/Ms [Name]",
-        farmLocation: { lat: 22.321512, lng: 52.42512 }
-    }
-    ,
-    {
-        farmId: "2",
-        farmName: "farm num 2",
-        farmOwner: "Mr/Ms [Name]",
-        farmLocation: { lat: 12.321512, lng: 122.42512 }
-    },
-    {
-        farmId: "3",
-        farmName: "farm num 3",
-        farmOwner: "Mr/Ms [Name]",
-        farmLocation: { lat: 32.321512, lng: 72.42512 }
-    },
-    {
-        farmId: "4",
-        farmName: "farm num 4",
-        farmOwner: "Mr/Ms [Name]",
-        farmLocation: { lat: 31.32132512, lng: 102.4234512 }
-    }]
 
 
 const users = [{ userId: "1", userName: "Hooman", userFname: "HJ", email: "hh@gmail.com", password: "12345", active: true }]
@@ -103,28 +73,28 @@ app.get('/V1/products/:productId', (req, res) => {
 
 // http://localhost:8080/V1/user/auth?name=hoomanhaj&email=hooman&pass=1234
 app.get('/V1/user/auth/reg', (req, res) => {
-    res.send(req.query);
     let name = req.query.name;
     let email = req.query.email;
     let pass = req.query.pass;
     console.log("email:" + email + " pass:" + pass + name);
-    cp
-        .then(pool => {
-            pool.query(`INSERT INTO user (user_name, user_email, user_password) VALUES ("${name}", ${email}, ${pass});`)
-                .then(result => {
-                    console.log("successfully registerd!")
-                })
-                .catch(error => res.status(500).send(error));
-        })
-        .catch(error => res.status(500).send(error));
+    res.send(req.query);
+    // cp
+    //     .then(pool => {
+    //         pool.query(`INSERT INTO user (user_name, user_email, user_password) VALUES ("${name}", ${email}, ${pass});`)
+    //             .then(result => {
+    //                 console.log("successfully registerd!")
+    //             })
+    //             .catch(error => res.status(500).send(error));
+    //     })
+    //     .catch(error => res.status(500).send(error));
 
 });
 
 app.get('/V1/user/auth/approval', (req, res) => {
-    res.send(req.query);
     let email = req.query.email;
     let pass = req.query.pass;
     console.log("email:" + email + " pass:" + pass)
+    res.send(req.query);
     // cp
     //     .then(pool => {
     //         pool.query(`SELECT * FROM product WHERE product_id=${req.params.productId}`)
@@ -141,6 +111,6 @@ app.get('/V1/user/auth/approval', (req, res) => {
 
 // });
 
-app.get('/*', () => {
-    res.status(404).send("page does not exist!");
+app.get('/*', (req, res) => {
+    res.status(404).send("Error404! page does not exist!");
 });
