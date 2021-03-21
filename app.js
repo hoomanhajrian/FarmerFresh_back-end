98eebb748d6b6b7a58470b8070f191fb7cd1f87e
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-03-21 00:52:14 -0700

    fixing

--- app.js
+++ app.js
@@ -97,8 +97,11 @@ app.get('/V1/addtocart', (req, res) => {
 
     cp
         .then(pool => {
-            pool.query(`INSERT INTO cart (user_id, farm_name, product_name, product_img, product_Fname, product_price, product_description, quantity) VALUES ("${mysql.escape(userId)}", "${mysql.escape(farmName)}", "${mysql.escape(pName)}", "${mysql.escape(pImg)}", "${mysql.escape(pFName)}", "${mysql.escape(pPrice)}", "${mysql.escape(pDescription)}", "${mysql.escape(quantity)}");`)
+
+            pool.query(`INSERT INTO cart (user_id, farm_name, product_name, product_img, product_Fname, product_price, product_description, quantity) VALUES (${mysql.escape(userId)}, ${mysql.escape(farmName)}, ${mysql.escape(pName)}, ${mysql.escape(pImg)}, ${mysql.escape(pFName)}, ${mysql.escape(pPrice)}, ${mysql.escape(pDescription)}, ${mysql.escape(quantity)});`)
+
                 .then(res.status(200).json.send({ "status": "200", "message": "Added Successfully", "User_ID": userId, "Farm_Name": farmName, "Product_Name": pName, "Product_Image": pImg, "Product_Family_Name": pFName, "Product_Price": pPrice, "Product_Description": pDescription, "Quantity": quantity }))
+
                 .catch(error => res.status(500).send(error));
         })
         .catch(error => res.status(500).send(error));
