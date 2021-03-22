8574ed434e359894492c1c1346a119b60cc1b51e
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-03-21 22:28:32 -0700

    fixing

--- app.js
+++ app.js
@@ -69,10 +69,10 @@ app.get('/V1/products/:productId', (req, res) => {
         .catch(error => res.status(500).send(error));
 });
 
-app.get('/V1/getfarmproducts?userid=:userId', (req, res) => {
+app.get('/V1/getfarmproducts?farmid=:farmid', (req, res) => {
     cp
         .then(pool => {
-            pool.query(`select * from farmerfresh.product where farmerfresh.product.product_id in (select product_product_id from farmerfresh.farm_has_product where farmerfresh.farm_has_product.farm_farm_id = ${req.params.farmId});`)
+            pool.query(`select * from farmerfresh.product where farmerfresh.product.product_id in (select product_product_id from farmerfresh.farm_has_product where farmerfresh.farm_has_product.farm_farm_id = ${req.params.farmid});`)
                 .then(result => {
                     res.send(result);
                 })
