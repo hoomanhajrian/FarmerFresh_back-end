4d0c384e3a68deaacc1b2679d70e7bbb1f5f03d9
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-03-21 23:30:19 -0700

    fixing

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/V1/farms', (req, res) => {
         .catch(error => res.status(500).send(error));
 });
 
-app.get('/V1/getfarmproducts/products', (req, res) => {
+app.get('/V1/getfarmproducts/products?farmId=:farmid', (req, res) => {
 
     cp
         .then(pool => {
