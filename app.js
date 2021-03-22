224c204ae02dff17eb273f766f09a63bfd4d5c3b
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-03-21 20:02:33 -0700

    fixing post

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const app = express();
 const { cp } = require('./db/connection.js');
 const mysql = require('promise-mysql');
 
-const users = [{ userId: "1", userName: "Hooman", userFname: "HJ", email: "hh@gmail.com", password: "12345", active: true }]
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 let server = app.listen(8080, () => {
     console.log('Server is listening on port 8080')
@@ -81,7 +79,7 @@ app.get('/V1/getfarmproducts?farmid=:farmId', (req, res) => {
         })
         .catch(error => res.status(500).send(error));
 });
-// api/V1/addtocart?user_id=(int)&farm_name=""&product_name=""&product_image="",product_family_name="",product_price="",product_description=""&quantity=""
+// https://farmerfresh.ca/api/V1/addtocart?user_id=1&farm_name=apple%20farm&product_name=apples&product_image=http:something&product_family_name=%22%22&product_price=%22%22&product_description=%22%22&quantity=%22%22
 app.get('/V1/addtocart', (req, res) => {
 
     let userId = req.query.user_id;
